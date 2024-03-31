4c5990ee053e1be755706ee515044a258a934dd8
yasoyuki <yuki1006yaso@gmail.com>
2024-03-31 21:47:22 +0900

    画像微修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ const Home = () => {
           <p className="border-b-4 border-red-500 pb-2 mb-5">ABOUT</p>
           <img src="../about.jpg" alt="自分の写真" className="w-64 md:w-80" />
           <span className="text-base pt-5 w-3/4 md:w-1/2">
-            私はWebライターおよびWeb制作のフリーランスとして活動した後、IT業界での事務職を経て、マークアップエンジニアとしてのキャリアを約1年間築いてきました。<br />
+            私はWebライターおよびWeb制作のフリーランスとして活動したのち、異業種での経験を経て、新たな視点を持ってまたIT業界へ戻って参りました。IT企業での事務職を約1年間、マークアップエンジニアとして約1年間キャリアを築いてきました。<br />
             フリーランス時代には、様々なクライアントのニーズに応えることで、コンテンツ制作とWebデザインの幅広いスキルを習得しました。その後、IT企業での事務職を通じて、組織運営の裏側を学び、チームワークとプロジェクト管理の重要性を実感しました。<br />
             最近では、マークアップエンジニアとしてHTML、CSS、JavaScriptを用いたフロントエンド開発に携わり、ユーザーインターフェースの重要性と、エンドユーザーにとっての使いやすさを常に念頭に置くことの大切さを学びました。これらの経験を基に、WebとITの分野でさらにスキルアップし、新しい挑戦を続けていきたいと考えています。</span>
         </section>
@@ -22,19 +22,31 @@ const Home = () => {
             <p className="border-b-4 border-red-500 pb-2 mb-5">WEB SITE</p>
             <div className="mt-3 px-5">
               <p className="text-xl text-left lg:text-2xl">コワーキングスペースのHP</p>
-              <ul className="text-base">
+              <ul className="text-base my-3">
                 <li>- LPページ</li>
                 <li>- レスポンシブ対応</li>
+                <li>- フレームワークはNext.jsを使用</li>
+                <li>- Typeacriptを採用</li>
               </ul>
             </div>
             <a href="https://website01-gold.vercel.app/">
               <button className="header hover:bg-blue-500 text-white px-8 border-none rounded-full flex items-center justify-center text-lg my-5 h-12">
                 <div className="text-2xl"><IoArrowDownCircleSharp /></div><p className="mt-2">　サイトを見る</p></button>
             </a>
-            <picture>
-              <source media="(min-width: 768px)" srcSet="../portfolio01-pc.jpg" />
-              <img src="../portfolio01-sp.jpg" alt="ポートフォリオサムネ" className="w-2/3 mx-auto md:w-4/5 lg:w-2/3" />
-            </picture>
+            <div className="lg:flex ">
+              <div className="w-11/12 mx-auto md:w-4/5 lg:w-3/5">
+                <p>PC</p>
+                <picture>
+                  <img src="../portfolio01-pc.jpg" alt="ポートフォリオサムネ" />
+                </picture>
+              </div>
+              <div className="w-2/5 mx-auto lg:w-1/5 mt-10 lg:mt-0">
+                <p>SP</p>
+                <picture>
+                  <img src="../portfolio01-sp.jpg" alt="ポートフォリオサムネ" />
+                </picture>
+              </div>
+            </div>
           </div>
         </section>
 
